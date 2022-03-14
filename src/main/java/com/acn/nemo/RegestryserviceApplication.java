47063f2965175a1ac13c534254d21ceaf40090df
Massimo Caputo <massimo.caputo@icloud.com>
2022-03-14 22:02:05 +0100

    Initial commit

---
+++ src/main/java/com/acn/nemo/RegestryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.acn.nemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegestryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegestryserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acn/nemo/RegestryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acn.nemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegestryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
